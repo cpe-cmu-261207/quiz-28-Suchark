dfc7489c2326960d54024582a86573ef68b9c10a
Your Name <you@example.com>
2022-08-04 14:13:16 +0700

    ok

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const DIVuserclass = document.querySelector("#div-user-card");
+const Imgpro = document.querySelector("#img-profile");
+const Pname = document.querySelector("#p-name");
+const btnRandom = document.querySelector("#btn-random");
+const Pemail = document.querySelector("#p-email");
+
+callApi();
+element.style.display = "none";
+function element() {
+  Loading.innerText = "Loading Profile...";
+}
+element.style.display = " ";
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  Imgpro.data.picture.large;
+  Pname.data.name;
 }
